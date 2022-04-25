041c9b9fab1468139b8d4689c1dc0109fd6e5b2e
flagro <antonpgrey@gmail.com>
2022-04-25 12:53:32 +0300

    added interface to work with polus

--- task1/result_printer.hpp
+++ task1/result_printer.hpp
@@ -1,5 +1,16 @@
 #pragma once
 
-void print_results() {
-    
+void print_results(int n, int test_id, double first_time, double second_time, double residual, double error) {
+    int threads_num = 0;
+    #pragma omp parallel
+    {
+        #pragma omp single
+        threads_num = omp_get_num_threads();
+    }
+    /*
+    FILE *my_f;
+    my_f = fopen("./output.txt", "a");
+    fprintf(my_f, "%d %d %d %.10lf %.10lf %.10lf %.10lf %.10lf\n", n, test_id, threads_num, first_time, second_time, first_time + second_time, residual, error);
+    fclose(my_f);
+    */
 }
--- task1/run_polus_tests.py
+++ task1/run_polus_tests.py
@@ -9,13 +9,36 @@ def get_bsub_input(threads_count, parameters_string):
         core_units = (threads_count + 1) // 2
     if threads_count > 40:
         print("Polus doesn't have more than 20 units, running ")
-    return f"""#BSUB -J \"OpenMP_job\"
-               #BSUB -W 0:15
-               #BSUB -o a.out.out
-               #BSUB -e a.out.err
-               #BSUB -R “affinity[core({core_units})]” OMP_NUM_THREADS={threads_count}
-               /polusfs/lsf/openmp/launchOpenMP.py ./a.out {parameters_string}"""
+    return (f"#BSUB -J \"OpenMP_job\"\n"
+            f"#BSUB -W 0:15\n"
+            f"#BSUB -o a.out.out\n"
+            f"#BSUB -e a.out.err\n"
+            f"#BSUB -R \"affinity[core({core_units})]\" OMP_NUM_THREADS={threads_count}\n"
+            f"/polusfs/lsf/openmp/launchOpenMP.py ./a.out {parameters_string}\n")
 
+
+def get_bsub_input_slow(threads_count, parameters_string):
+    if threads_count > 160:
+        print("Warning: Polus can't run more than 160 omp threads, using 160 instead")
+        threads_count = 160
+    core_units = threads_count // 8 + 1
+    return (f"#BSUB -n {core_units}\n"
+            f"#BSUB -W 0:15\n"
+            f"#BSUB -o a.out.out\n"
+            f"#BSUB -e a.out.err\n"
+            f"#BSUB -R \"span[hosts=1]\"\n"
+            f"OMP_NUM_THREADS={threads_count} ./a.out {parameters_string}\n")
+
+
+def get_bsub_input_fast(threads_count, parameters_string):
+    if threads_count > 20:
+        threads_count = 20
+    return (f"#BSUB -J \"OpenMP_job\"\n"
+            f"#BSUB -W 0:15\n"
+            f"#BSUB -o a.out.out\n"
+            f"#BSUB -e a.out.err\n"
+            f"#BSUB -R \"affinity[core({threads_count})]\"\n"
+            f"/polusfs/lsf/openmp/launchOpenMP.py ./a.out\n {parameters_string}")
 
 
 test_matrix_sizes = [2, 4, 10, 100, 1000, 2000, 3000]
@@ -26,4 +49,7 @@ test_test_ids = valid_test_ids[:1]
 for matrix_size in test_matrix_sizes:
     for threads_count in test_threads_counts:
         for test_id in test_test_ids:
-            print(get_bsub_input(threads_count, f"{matrix_size} {threads_count} {test_id} --residual --polus"))
+            f = open("./OpenMP_job.lsf", "w")
+            f.write(get_bsub_input(threads_count, f"{matrix_size} {threads_count} {test_id} --residual --polus"))
+            f.close()
+            #os.system("bsub < OpenMP_job.lsf")
