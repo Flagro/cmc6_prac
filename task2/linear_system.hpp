48ad6ca703f63e53854b1f5015bcf0e31b0ed4b1
flagro <antonpgrey@gmail.com>
2022-05-14 16:48:29 +0300

    implemented mpi base for linear system

--- task2/linear_system.hpp
+++ task2/linear_system.hpp
@@ -1,7 +1,7 @@
 #pragma once
 #include <iostream>
 #include <cmath>
-#include "omp.h"
+#include <mpi.h>
 #include "matrix.hpp"
 #include "vector.hpp"
 #include "cramer_rule.hpp"
@@ -17,19 +17,11 @@ public:
         }
     }
 
-    std::unique_ptr<Vector<result_T> > solve_reflection_method(int threads_num, bool mpi_machine_used, 
-                                                               double *first_stage_elapsed_time, 
+    std::unique_ptr<Vector<result_T> > solve_reflection_method(double *first_stage_elapsed_time, 
                                                                double *second_stage_elapsed_time) {
-        if (!mpi_machine_used) {
-            std::cout << "turning off omp set dynamic" << std::endl;;
-            omp_set_dynamic(0);
-            omp_set_num_threads(threads_num);
-        }
-        #pragma omp parallel
-        {
-            #pragma omp single
-            std::cout << "Solving linear system with Reflection Method using " << omp_get_num_threads() << " threads" << std::endl;
-        }
+        int world_rank;
+        MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
         std::vector<std::vector<result_T> > result_matrix(_n, std::vector<result_T>(_n, 0));
         for (size_t i = 0; i < _n; ++i) {
             for (size_t j = 0 ; j < _n; ++j) {
@@ -41,10 +33,13 @@ public:
             result_vector[i] = _b.get(i);
         }
         std::vector<result_T> x(_n, 0);
-        auto first_stage_elapsed_time_first = omp_get_wtime();
+
+        MPI_Barrier(MPI_COMM_WORLD);
+        auto first_stage_elapsed_time_first = MPI_Wtime();
+
         for (size_t i = 0; i + 1 < _n; ++i) {
             result_T s = 0;
-            #pragma omp parallel for reduction(+:s) 
+            //#pragma omp parallel for reduction(+:s) 
             for (size_t j = i + 1; j < _n; ++j) {
                 s += result_matrix[j][i] * result_matrix[j][i];
             }
@@ -52,27 +47,27 @@ public:
             result_T cur_x1 = result_matrix[i][i] - cur_a_norm;
             result_T cur_x_norm = sqrt(cur_x1 * cur_x1 + s);
             x[i] = cur_x1 / cur_x_norm;
-            #pragma omp parallel for
+            //#pragma omp parallel for
             for (size_t j = i + 1; j < _n; ++j) {
                 x[j] = result_matrix[j][i] / cur_x_norm;
             }
             // doing mxv (U(x)b) = (I-2xx^*)b = b - 2x(b, x)
             result_T alpha = 0;
-            #pragma omp parallel for reduction(+:alpha) 
+            //#pragma omp parallel for reduction(+:alpha) 
             for (size_t j = i; j < _n; ++j) {
                 alpha += result_vector[j] * x[j];
             }
-            #pragma omp parallel for
+            //#pragma omp parallel for
             for (size_t j = i; j < _n; ++j) {
                 result_vector[j] -= 2 * x[j] * alpha;
             }
             // doing mxv (U(x)A_{*j}) = A_{*j} - 2x(A_{*j}, x) for each j-th column from A
             result_matrix[i][i] = cur_a_norm;
-            #pragma omp parallel for
+            //#pragma omp parallel for
             for (size_t j = i + 1; j < _n; ++j) {
                 result_matrix[j][i] = 0;
             }
-            #pragma omp parallel for
+            //#pragma omp parallel for
             for (size_t j = i + 1; j < _n; ++j) {
                 result_T local_alpha = 0;
                 for (size_t k = i; k < _n; ++k) {
@@ -84,14 +79,18 @@ public:
             }
         }
 
-        auto first_stage_elapsed_time_second = omp_get_wtime();
-        std::cout << "Triangular form:" << std::endl;
-        ArrayMatrix<result_T>(_n, result_matrix).print(TRIANGULAR_MATRIX_PRINT_COUNT);
-        ArrayVector<result_T>(_n, result_vector).print(TRIANGULAR_MATRIX_PRINT_COUNT);
-        std::cout << std::endl;
-
+        MPI_Barrier(MPI_COMM_WORLD);
+        auto first_stage_elapsed_time_second = MPI_Wtime();
+        if (!world_rank) {
+            std::cout << "Triangular form:" << std::endl;
+            ArrayMatrix<result_T>(_n, result_matrix).print(TRIANGULAR_MATRIX_PRINT_COUNT);
+            ArrayVector<result_T>(_n, result_vector).print(TRIANGULAR_MATRIX_PRINT_COUNT);
+            std::cout << std::endl;
+        }
         std::vector<result_T> result_array(_n, 0);
-        auto second_stage_elapsed_time_first = omp_get_wtime();
+
+        MPI_Barrier(MPI_COMM_WORLD);
+        auto second_stage_elapsed_time_first = MPI_Wtime();
 
         for (int i = (int)_n - 1; i >= 0; --i) {
             result_array[i] = result_vector[i] / result_matrix[i][i];
@@ -99,7 +98,8 @@ public:
                 result_vector[j] -= result_matrix[j][i] * result_array[i];
             }
         }
-        auto second_stage_elapsed_time_second = omp_get_wtime();
+        MPI_Barrier(MPI_COMM_WORLD);
+        auto second_stage_elapsed_time_second = MPI_Wtime();
 
         *first_stage_elapsed_time = first_stage_elapsed_time_second - first_stage_elapsed_time_first;
         *second_stage_elapsed_time = second_stage_elapsed_time_second - second_stage_elapsed_time_first;
@@ -109,7 +109,6 @@ public:
 
     double calculate_residual(const Vector<result_T>& x) {
         std::vector<result_T> residual(_n, 0);
-        #pragma omp parallel for
         for (size_t i = 0; i < _n; ++i) {
             for (size_t j = 0; j < _n; ++j) {
                 residual[i] += _A.get(i, j) * x.get(j);
@@ -117,7 +116,6 @@ public:
             residual[i] -= _b.get(i);
         }
         result_T residual_norm = 0;
-        #pragma omp parallel for reduction(+:residual_norm) 
         for (size_t i = 0; i < _n; ++i) {
             residual_norm += residual[i] * residual[i];
         }
