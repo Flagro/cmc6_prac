ee9b745620404dcd1b309b6bf031cc06873f495a
flagro <antonpgrey@gmail.com>
2022-05-23 11:44:56 +0300

    implemented ellpack generation

--- task3/formula_generators.hpp
+++ task3/formula_generators.hpp
@@ -22,16 +22,48 @@ std::vector<CubeMatrixGenerator<double> > cube_matrix_function_generators = {
     {[](size_t n_x, size_t n_y, size_t n_z) {
         static const size_t M = 7;
         static const double PI = 3.14;
+        static const auto non_diagonal_value = [](size_t i, size_t j) { return cos(i * j + PI); };
+
         size_t n = n_x * n_y * n_z;
+
         EllpackMatrix<double> result;
         result.n = n;
         result.m = M;
         result.ellpack_col = std::vector<std::vector<size_t> >(n, std::vector<size_t>(M));
-        result.ellpack_val = std::vector<std::vector<double> >(n, std::vector<double>(M));
-        for (size_t i = 0; i < n; ++i) {
-            for (size_t j = 0; j < M; ++j) {
-                result.ellpack_col[i][j];
-                result.ellpack_val[i][j];
+        result.ellpack_val = std::vector<std::vector<double> >(n, std::vector<double>(M, 0.0));
+        for (size_t i = 0; i < n_x; ++i) {
+            for (size_t j = 0; j < n_y; ++j) {
+                for (size_t k = 0; k < n_z; ++k) {
+                    size_t cur_row = k * n_x * n_y + j * n_x + i;
+                    std::vector<std::pair<size_t, bool> > cur_cols_ids = 
+                        {{cur_row - n_x * n_y, k > 0}, 
+                         {cur_row - n_x, j > 0}, 
+                         {cur_row - 1, i > 0}, 
+                         {cur_row, true}, 
+                         {cur_row + 1, i < n_x - 1}, 
+                         {cur_row + n_x, j < n_y - 1}, 
+                         {cur_row + n_x * n_y, k < n_z - 1}};
+
+                    size_t cur_col_offset = 0;
+                    size_t diagonal_el_offset = 0;
+                    double non_diagonal_norm_sum = 0;
+                    for (const auto & [cur_col, to_use] : cur_cols_ids) {
+                        if (to_use) {
+                            result.ellpack_col[cur_row][cur_col_offset] = cur_col;
+                            if (cur_row == cur_col) {
+                                diagonal_el_offset = cur_col_offset;
+                            } else {
+                                result.ellpack_val[cur_row][cur_col_offset] = non_diagonal_value(cur_row, cur_col);
+                                non_diagonal_norm_sum += fabs(result.ellpack_val[cur_row][cur_col_offset]);
+                            }
+                            ++cur_col_offset;
+                        }
+                    }
+                    result.ellpack_val[cur_row][diagonal_el_offset] = 1.5 * non_diagonal_norm_sum;
+                    for (size_t zero_val_id = cur_col_offset; zero_val_id < M; ++zero_val_id) {
+                        result.ellpack_col[cur_row][zero_val_id] = n + 1 + zero_val_id - cur_col_offset;
+                    }
+                }
             }
         }
         return result;
--- task3/matrix.hpp
+++ task3/matrix.hpp
@@ -12,8 +12,8 @@ public:
 
     const T & get(size_t i, size_t j) const {
         static const T zero_val = 0;
-        auto lower_bound_it = std::lower_bound(_ellpack_data.ellpack_col[i].begin(), _ellpack_data.ellpack_col[i].begin(), j);
-        if (lower_bound_it != _ellpack_data.ellpack_col[i].begin() && *lower_bound_it == j) {
+        auto lower_bound_it = std::lower_bound(_ellpack_data.ellpack_col[i].begin(), _ellpack_data.ellpack_col[i].end(), j);
+        if (lower_bound_it != _ellpack_data.ellpack_col[i].end() && *lower_bound_it == j) {
             return _ellpack_data.ellpack_val[i][lower_bound_it - _ellpack_data.ellpack_col[i].begin()];
         }
         return zero_val;
@@ -56,7 +56,7 @@ public:
                 }
                 std::cout << std::endl;
             }
-            std::cout << ". . . ";
+            std::cout << ". . . " << std::endl;
             for (size_t i = _n - print_count - 1; i < _n; ++i) {
                 for (size_t j = 0; j < print_count; ++j) {
                     std::cout << get(i, j) << " ";
